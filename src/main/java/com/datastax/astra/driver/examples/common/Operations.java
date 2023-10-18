c1dbaba5aa70816a8aeab4f075606d2a60610a8e
Henry Hughes <henry.hughes@datastax.com>
2023-10-18 10:59:41 -0700

    Implement query tracing

--- src/main/java/com/datastax/astra/driver/examples/common/Operations.java
+++ src/main/java/com/datastax/astra/driver/examples/common/Operations.java
@@ -7,7 +7,9 @@ import com.datastax.oss.driver.api.core.DriverTimeoutException;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.connection.ClosedConnectionException;
 import com.datastax.oss.driver.api.core.cql.BoundStatementBuilder;
+import com.datastax.oss.driver.api.core.cql.ExecutionInfo;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.QueryTrace;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import com.datastax.oss.driver.api.core.cql.Statement;
@@ -18,10 +20,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Operations {
     private static final boolean USE_NEW_TABLE = false;
@@ -120,14 +126,32 @@ public class Operations {
     public static ResultSet runWithRetries(CqlSession session, Statement query) {
         // Queries will be retried indefinitely on timeout, they must be idempotent
         // In a real application there should be a limit to the number of retries
+        Statement traced = query.setTracing(true);
         while (true) {
             try {
-                return session.execute(query);
+                LOG.trace("Executing query: {}", traced);
+                long qStart = System.nanoTime();
+                ResultSet resultSet = session.execute(traced);
+                long qFinish = System.nanoTime();
+
+                LOG.trace("Completed query: {} in {}µs", traced, TimeUnit.NANOSECONDS.toMicros(qFinish - qStart));
+
+                QueryTrace queryTrace = resultSet.getExecutionInfo().getQueryTrace();
+                LOG.trace("Query trace: {}", queryTrace);
+                queryTrace.getEvents().forEach(event ->
+                        LOG.trace("* {} on {}[{}] at {} ({}µs)",
+                                event.getActivity(),
+                                event.getSourceAddress(),
+                                event.getThreadName(),
+                                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date(event.getTimestamp())),
+                                event.getSourceElapsedMicros()));
+
+                return resultSet;
             } catch (DriverTimeoutException | WriteTimeoutException | ReadTimeoutException | ClosedConnectionException e) {
                 // request timed-out, catch error and retry
-                LOG.warn(String.format("Error '%s' executing query '%s', retrying", e.getMessage(), query), e);
+                LOG.warn(String.format("Error '%s' executing query '%s', retrying", e.getMessage(), traced), e);
             } catch (AllNodesFailedException e) {
-                LOG.error(String.format("AllNodesFailedException error '%s' executing query '%s', retrying", e.getMessage(), query), e);
+                LOG.error(String.format("AllNodesFailedException error '%s' executing query '%s', retrying", e.getMessage(), traced), e);
             }
         }
     }
@@ -141,6 +165,7 @@ public class Operations {
             } catch (AllNodesFailedException e) {
                 // session creation failed, probably due to time-out, catch error and retry
                 LOG.warn("Failed to create session.", e);
+                throw e;
             }
         }
     }
